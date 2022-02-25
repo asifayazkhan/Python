491fc6d24ad904748f09675204ac24cc37ab504e
asifayazkhan <asif.ayazkhan521@gmail.com>
2022-02-25 20:23:49 +0500

    adding turtle file

---
+++ Python mini projects/turtle_shape.py
@@ -0,0 +1,11 @@
+# Draw star shape with python turtle
+from turtle import *
+color('red', 'yellow')
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
